f21d48c091ccf2e0dd4a17ecc8b50f4548882e0a
sorwar99 <stasoul18@jaumebalmes.net>
2020-02-10 20:00:18 +0100

    prova

---
+++ APPS/Municipis/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Sorwar Hussian</title>
+</head>
+
+<body>
+    <div class="container">
+        <header class="bg-secondary p-2 text-white">
+            <h1 class="text-center">Comunitats, províncies, municipis. AJAX i bases de dades</h1>
+            <h2 class="text-center">Autor: Sorwar Hussian</h2>
+        </header>
+
+        <?php
+
+        $servername = "localhost";
+        $username = "sorwar";
+        $password = "sorwar99";
+        $port = 8889;
+        $myDB = "jbalmes";
+
+        try {
+            $dbh = new PDO("mysql:host=$servername; dbname=$myDB; port=$port", $username, $password);
+
+            $stmt = $dbh->prepare('SELECT * FROM comunitats  ');
+            $stmt ->execute();
+
+            $resultados = $stmt->fetchAll();
+            echo "<pre>";
+            print_r($resultados);
+        } catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+            
+        }
+
+
+
+
+        ?>
+
+    </div>
+
+</body>
+
+</html>
